06ffc32a8ac71cd9e35189b76c0ff6018c963d96
kmga4 <31944445+kmga4@users.noreply.github.com>
2017-10-09 16:19:05 +0900

    김민겸 day6_hw.py

---
+++ 김민겸 day6_hw.py
@@ -0,0 +1,105 @@
+import turtle as t
+t.shape("turtle")
+t.color("violet")
+t.pensize(6)
+t.speed(0)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.lt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.lt(26)
+for x in range(10):
+    t.forward(10)
+    t.lt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(10):
+    t.forward(10)
+    t.lt(26)
+for x in range(10):
+    t.forward(10)
+    t.lt(26)
+for x in range(10):
+    t.forward(10)
+    t.rt(26)
+for x in range(5):
+    t.forward(10)
+    t.rt(26)
+for x in range(8):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.right(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+for x in range(10):
+    t.forward(10)
+    t.left(26)
+t.color("brown")
+t.pensize(3)
+t.rt(90)
+t.fd(50)
+t.rt(90)
+t.fd(20)
+t.rt(90)
+t.fd(50)
+t.rt(90)
+t.fd(20)
